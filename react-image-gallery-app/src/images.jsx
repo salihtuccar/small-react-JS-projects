4cf46ce9c45689500b6aeeba4e3a6166b295082a
salihtuccar <salihtuccar1@gmail.com>
2024-04-09 22:27:53 +0200

    .

--- react-image-gallery-app/src/images.jsx
+++
@@ -1,7 +0,0 @@
-export default [
-  "https://picsum.photos/200/300",
-  "https://imgrosetta.mynet.com.tr/file/11990709/11990709-728xauto.jpg",
-  "https://media-cdn.tripadvisor.com/media/photo-s/0e/ac/1d/e7/ekmek-tantuni.jpg",
-  "https://i.ytimg.com/vi/ZsuDMqjEcCQ/maxresdefault.jpg",
-  "https://cdn.getiryemek.com/products/1552376081831_500x375.png",
-];
