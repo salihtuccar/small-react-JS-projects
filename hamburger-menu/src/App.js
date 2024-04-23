ff8c3e0f8881e87adf516ae81d59ee42817f9b08
salihtuccar <salihtuccar1@gmail.com>
2024-04-23 11:58:02 +0200

    .

--- hamburger-menu/src/App.js
+++ hamburger-menu/src/App.js
@@ -1,46 +1,11 @@
+import React from "react";
 import "./App.css";
-import React, { useState } from "react";
+import Navbar from "./Navbar.js";
 
 function App() {
-  const [newItem, setNewItem] = useState("");
-  const [items, setItems] = useState([]);
-  function deleteItem(id) {
-    const newArray = items.filter((item) => item.id !== id);
-    setItems(newArray);
-  }
-  function addItem() {
-    if (!newItem) {
-      alert("Enter an item");
-      return;
-    }
-    const item = {
-      id: Math.floor(Math.random() * 1000),
-      value: newItem,
-    };
-    setItems((oldItems) => [...oldItems, item]);
-    setNewItem("");
-  }
   return (
     <div className="App">
-      <h1>Todo List App</h1>
-      <input
-        type="text"
-        placeholder="Add An Item..."
-        value={newItem}
-        onChange={(e) => setNewItem(e.target.value)}
-      />
-      <button onClick={() => addItem()}>Add</button>
-      <ul>
-        {items.map((item) => {
-          return (
-            <li key={item.id}>
-              {" "}
-              {item.value}{" "}
-              <button onClick={() => deleteItem(item.id)}>X</button>
-            </li>
-          );
-        })}
-      </ul>
+      <Navbar />
     </div>
   );
 }
