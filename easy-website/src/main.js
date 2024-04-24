d72a95638934e38bf26bce582513b0f6951c56f7
salihtuccar <salihtuccar1@gmail.com>
2024-04-24 16:44:58 +0200

    .

---
+++ easy-website/src/main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./main.css";
+export default function () {
+  return (
+    <div className="site-info">
+      <div className="title">SalihTuccar</div>
+      <div className="sub-title">WEB DEVOLOPER</div>
+    </div>
+  );
+}
