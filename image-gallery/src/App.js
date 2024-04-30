04323009e4a8cdfec9d967577b777dad2aa91a56
salihtuccar <salihtuccar1@gmail.com>
2024-04-30 21:58:22 +0200

    .

---
+++ image-gallery/src/App.js
@@ -0,0 +1,15 @@
+import Gallery from "./Gallery";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="mainH1" style={{ textAlign: "center" }}>
+        Image Gallery
+      </h1>
+      <Gallery />
+    </>
+  );
+}
+
+export default App;
